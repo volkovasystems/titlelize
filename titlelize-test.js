2fffec12d338882120b34fea5aaa2dfa69934632
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-09 12:57:35 +0800

    Updated test.

--- titlelize-test.js
+++ titlelize-test.js
@@ -2,14 +2,14 @@
 const assert = require( "assert" );
 const titlelize = require( "./titlelize.js" );
 
-assert.equal( titlelize( "helloworld" ), "Helloworld", "should have value 'Helloworld'" );
+assert.equal( titlelize( "helloworld" ), "Helloworld", "should be equal to 'Helloworld'" );
 
-assert.equal( titlelize( "hello-world" ), "Hello World", "should have value 'Hello World'" );
+assert.equal( titlelize( "hello-world" ), "Hello World", "should be equal to 'Hello World'" );
 
-assert.equal( titlelize( "hello_world" ), "Hello World", "should have value 'Hello World'" );
+assert.equal( titlelize( "hello_world" ), "Hello World", "should be equal to 'Hello World'" );
 
-assert.equal( titlelize( "helloWorld" ), "Hello World", "should have value 'Hello World'" );
+assert.equal( titlelize( "helloWorld" ), "Hello World", "should be equal to 'Hello World'" );
 
-assert.equal( titlelize( "hello world" ), "Hello World", "should have value 'Hello World'" );
+assert.equal( titlelize( "hello world" ), "Hello World", "should be equal to 'Hello World'" );
 
 console.log( "ok" );
